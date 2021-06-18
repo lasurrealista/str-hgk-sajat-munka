c60fd7ecfa546755d765b71783c2988301e021b6
Evelin <ladyblue@freemail.hu>
2021-06-18 13:52:56 +0200

    nodejs-alapjai-yargs-1-after-async-await

---
+++ nodejs-alapjai-yargs/1/after/app.js
@@ -0,0 +1,61 @@
+const yargs = require('yargs');
+const { dbFilePath, propName} = require('./config');
+const { id, producer, title } = require('./option');
+const MoviesApi = require('./movies.api');
+const MoviesService = require('./movies.service');
+
+const moviesApi = MoviesApi(dbFilePath, propName);
+const {
+  getAllMovies,
+  findMovieById,
+  createMovie,
+  editMovie,
+  removeMovie
+} = MoviesService(moviesApi);
+
+yargs
+  .version('1.0.0')
+  .usage('Usage: <command> [options]')
+  .command({
+    command: 'get',
+    describe: 'Get all movies',
+    handler: async () => console.log(await getAllMovies())
+  })
+  .command({
+    command: 'find',
+    describe: 'Find a movie by id',
+    builder: { id },
+    handler: async (args) =>
+      console.log(await findMovieById(args.id))
+  })
+  .command({
+    command: 'create',
+    describe: 'Create a new movie',
+    builder: { producer, title },
+    handler: async (args) => {
+      console.log(await createMovie(args))
+    }
+  })
+  .command({
+    command: 'edit',
+    describe: 'Edit a movie',
+    builder: { id, producer, title },
+    handler: async (args) => {
+      console.log(await editMovie(args))
+    }
+  })
+  .command({
+    command: 'remove',
+    describe: 'Remove a movie by ID',
+    builder: { id },
+    handler: async (args) => {
+      await removeMovie(args.id)
+      console.log('deleted')
+    }
+  })
+  .locale('en')
+  .strict()
+  .help()
+  .parse()
+
+  //async-await; dbFilePath, propName
---
+++ nodejs-alapjai-yargs/1/after/movies.api.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const MoviesApi = (path, prop) => ({
+  get () {
+    const dataString = readFileSync(path);
+    return JSON.parse(dataString)[prop];
+  },
+
+  save (data) {
+    writeFileSync(path, JSON.stringify({ [prop]: data }));
+  }
+})
+
+module.exports = MoviesApi;
+
+//async-await, readFileSync, writeFileSync, promises
