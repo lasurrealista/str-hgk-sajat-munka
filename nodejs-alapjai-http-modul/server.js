0e7f451279cbaa72ab5df08a1b371bb5064d2153
Evelin <ladyblue@freemail.hu>
2021-06-30 11:24:13 +0200

    nodejs-alapjai-http-modul-logger-megoldas

--- nodejs-alapjai-http-modul/server.js
+++ nodejs-alapjai-http-modul/server.js
@@ -1,12 +1,17 @@
 const http = require('http');
 const SiteRouter = require('./routers/site.router');
+const logger = require('./utils/logger')
 
 const port = 8080;
 
-http.createServer(({ url }, res) => {
+http.createServer(({ url, method }, res) => {
   SiteRouter[url]
     ? SiteRouter[url](res)
     : SiteRouter['/404'](res)
-}).listen(port);
+    logger(url, method)
+})
+//.on('error', (err) => console.log(`Server error: ${err.message}`))
+//.on('listening', () => console.log(`Server is running at http://127.0.0.1:${port}`))
+.listen(port);
 
 console.log(`Server is running at http://127.0.0.1:${port}`);
