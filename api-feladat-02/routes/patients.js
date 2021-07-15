d210aab2db76a2ced07176d7bd8ed5d15f1c1148
Evelin <ladyblue@freemail.hu>
2021-07-15 18:21:59 +0200

    Patient is vaccinated

--- api-feladat-02/routes/patients.js
+++ api-feladat-02/routes/patients.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const patientService = require('../service/patient.service');
 
+//http-errors: npm i http-errors -S 
+const createError = require("http-errors");
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
     const patients = await patientService.read();
@@ -23,6 +26,22 @@ router.get('/vaccinated', async (req, res, next) => {
     const patients = await patientService.read();
     const vaccinatedPatients = patients.filter( (patient) => patient.vaccine );
     res.json(vaccinatedPatients);
+  });
+
+// http://localhost:3000/patients/1/vaccinated
+router.get('/:id/vaccinated', async (req, res, next) => {
+
+    const patients = await patientService.read();
+    const { id } = req.params;
+    const patient = patients.find( patient => patient.id === parseInt(id, 10));
+  
+    if (!patient) {
+        return next(new createError.NotFound("Patient not found"));
+    }
+
+    res.json(
+        patient.vaccine === "" ? false : true);
+    
   });
 
 module.exports = router;
