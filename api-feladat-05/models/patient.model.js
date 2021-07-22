3d3963778d9df2d2690d2855dd8bdbded80cbc36
Evelin <ladyblue@freemail.hu>
2021-07-22 15:12:30 +0200

    Vaccine Schema

--- api-feladat-05/models/patient.model.js
+++ api-feladat-05/models/patient.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-const PatientSchema = mongoose.Schema( 
-    {
-        _id: Number,
-        firstName: String,
-        lastName: String,
-        vaccine: String,
-    }, 
-    {
-        timestamps: true,
-    }
-);
+const PatientSchema = mongoose.Schema({
+    _id: Number,
+    firstName: String,
+    lastName: String,
+    vaccine: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vaccine',
+    },
+    count: Number,
+}, {
+    timestamps: true,
+});
 
 module.exports = mongoose.model('Patient', PatientSchema);
