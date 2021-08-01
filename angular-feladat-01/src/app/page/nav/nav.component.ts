8c87080c5d435000800217b5c451c22828838301
Evelin <ladyblue@freemail.hu>
2021-08-01 13:38:03 +0200

    Login + navigation

--- angular-feladat-01/src/app/page/nav/nav.component.ts
+++ angular-feladat-01/src/app/page/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
 import { ConfigService } from 'src/app/service/config.service';
 
 @Component({
@@ -10,11 +13,27 @@ export class NavComponent implements OnInit {
 
   navigation = this.config.navigation;
 
+  loginStatus = false;
+  userSub: Subscription = new Subscription();
+  user: User | null = null;
+
   constructor(
-    private config: ConfigService
+    private config: ConfigService,
+    private auth: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.userSub = this.auth.currentUserSubject.subscribe(
+      user => this.user = user
+    );
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  onLogout() {
+    this.auth.logout();
   }
 
 }
